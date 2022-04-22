ae77fcdf026740d333a8f1e55dd6e50e90ef3748
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-04-22 22:32:06 +0200

    Add export of Description custom annotation to module-info.java

--- ServiceProvider/src/main/java/module-info.java
+++ ServiceProvider/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module se.iths.inventory.service {
     exports se.iths.inventory.entity;
     exports se.iths.inventory.interfaces;
     exports se.iths.inventory.service;
+    exports se.iths.inventory.annotation;
     uses se.iths.inventory.interfaces.Inventory;
 }

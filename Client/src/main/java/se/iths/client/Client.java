e1fabddd7387648a1dafb7056af6d88fa4bcdcec
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-04-25 18:48:11 +0200

    Refactor Client.java

--- Client/src/main/java/se/iths/client/Client.java
+++ Client/src/main/java/se/iths/client/Client.java
@@ -8,49 +8,49 @@ import se.iths.inventory.service.InventoryService;
 import java.lang.annotation.Annotation;
 
 public class Client {
-    public static void main(String[] args) {
-        InventoryService service = InventoryService.getInstance();
+    private static final InventoryService service = InventoryService.getInstance();
 
+    public static void main(String[] args) {
         System.out.println("""
                 Check Inventory descriptions demo
                 ----------------------""");
-        requestAnnotations("Body", service);
-        requestAnnotations("Electrical", service);
-        requestAnnotations("NONE", service);
+        requestAnnotations("Body");
+        requestAnnotations("Electrical");
+        requestAnnotations("NONE");
 
         System.out.println("""
                                 
                 Parts by ID demo
                 ----------------------""");
-        requestPartByID(1L, service);
-        requestPartByID(4L, "ELECTRICAL_AND_IGNITION", service);
-        requestPartByID(4L, "BODY AND FRAME", service);
+        requestPartByID(1L);
+        requestPartByID(4L, "ELECTRICAL AND IGNITION");
+        requestPartByID(4L, "BODY AND FRAME");
 
         System.out.println("""
                                 
                 Parts by Name demo
                 ----------------------""");
-        requestPartsByName("Sensor cable", service);
-        requestPartsByName("Handlebar", "BODY AND FRAME", service);
+        requestPartsByName("Sensor cable");
+        requestPartsByName("Handlebar", "BODY AND FRAME");
 
         System.out.println("""
                                 
                 Parts by Position demo
                 ----------------------""");
-        requestPartsByPosition(1, 8, service);
-        requestPartsByPosition(1, 0, "ELECTRICAL_AND_IGNITION", service);
+        requestPartsByPosition(1, 8);
+        requestPartsByPosition(1, 0, "ELECTRICAL AND IGNITION");
 
         System.out.println("""
                                 
                 Get All demo
                 ----------------------""");
-        requestAll(service);
-        requestAllPartsByCategory("BODY AND FRAME", service);
-        requestAllPartsByCategory("ELECTRICAL_AND_IGNITION", service);
-        requestAllPartsByCategory("NONE EXISTING", service);
+        requestAll();
+        requestAllPartsByCategory("BODY AND FRAME");
+        requestAllPartsByCategory("ELECTRICAL AND IGNITION");
+        requestAllPartsByCategory("NONE EXISTING");
     }
 
-    private static void requestAnnotations(String name, InventoryService service) {
+    private static void requestAnnotations(String name) {
         service.getAnnotations(name)
                 .ifPresentOrElse(annotations -> {
                     String collectedName = "";
@@ -73,49 +73,49 @@ public class Client {
 
     }
 
-    private static void requestPartByID(Long id, InventoryService service) {
+    private static void requestPartByID(Long id) {
         service.getPartByID(id)
                 .ifPresentOrElse(part -> System.out.println("The Part with ID '" + id + "' was found, here are the details: " + part),
                         () -> System.out.println("Part with ID '" + id + "' not found in Inventory"));
     }
 
-    private static void requestPartByID(Long id, String category, InventoryService service) {
+    private static void requestPartByID(Long id, String category) {
         service.getPartByID(id, category)
                 .ifPresentOrElse(part -> System.out.println("The Part with ID '" + id + "' was found in '" + category + "', here are the details: " + part),
                         () -> System.out.println("Part with ID '" + id + "' not found in '" + category + "' Inventory"));
     }
 
-    private static void requestPartsByName(String name, InventoryService service) {
+    private static void requestPartsByName(String name) {
         service.getPartsByName(name)
                 .ifPresentOrElse(parts -> System.out.println("Parts with name containing '" + name + "' was found, here are the details: " + parts),
                         () -> System.out.println("Parts with name containing '" + name + "' not found in Inventory"));
     }
 
-    private static void requestPartsByName(String name, String category, InventoryService service) {
+    private static void requestPartsByName(String name, String category) {
         service.getPartsByName(name, category)
                 .ifPresentOrElse(parts -> System.out.println("Parts with name containing '" + name + "' was found in '" + category + "', here are the details: " + parts),
                         () -> System.out.println("Parts with name containing '" + name + "' not found in '" + category + "' Inventory"));
     }
 
-    private static void requestPartsByPosition(int cabinet, int shelf, InventoryService service) {
+    private static void requestPartsByPosition(int cabinet, int shelf) {
         service.getPartByPosition(cabinet, shelf)
                 .ifPresentOrElse(parts -> System.out.println("Parts in cabinet '" + cabinet + "' and on shelf '" + shelf + "' was found, here are the details: " + parts),
                         () -> System.out.println("Parts in cabinet '" + cabinet + "' and on shelf '" + shelf + "' not found in Inventory"));
     }
 
-    private static void requestPartsByPosition(int cabinet, int shelf, String category, InventoryService service) {
+    private static void requestPartsByPosition(int cabinet, int shelf, String category) {
         service.getPartByPosition(cabinet, shelf, category)
                 .ifPresentOrElse(parts -> System.out.println("Parts in cabinet '" + cabinet + "' and on shelf '" + shelf + "' was found in '" + category + "', here are the details: " + parts),
                         () -> System.out.println("Parts in cabinet '" + cabinet + "' and on shelf '" + shelf + "' not found in '" + category + "' Inventory"));
     }
 
-    private static void requestAll(InventoryService service) {
+    private static void requestAll() {
         service.getAll()
                 .ifPresentOrElse(parts -> System.out.println("Returning all parts in all categories, here are the details: " + parts),
                         () -> System.out.println("No parts found!"));
     }
 
-    private static void requestAllPartsByCategory(String category, InventoryService service) {
+    private static void requestAllPartsByCategory(String category) {
         service.getAllByCategory(category)
                 .ifPresentOrElse(parts -> System.out.println("Parts in '" + category + "' inventory found, here are the details: " + parts),
                         () -> System.out.println("Inventory '" + category + "' not found, or empty!"));
